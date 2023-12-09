47a7dfb49fbe8012df72f720ebceff4a6fb7be7d
Adam Israel <adam@adamisrael.com>
2023-12-09 18:17:40 -0500

    Add Criterion for benchmarking
    Benchmarking performance via the Criterion crate. Run `cargo bench` to get a baseline, and re-run after coding to see variations in performance.

---
+++ benches/parse_gedcom.rs
@@ -0,0 +1,16 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use gedcom_test::parse::parse_gedcom;
+
+use std::time::Duration;
+
+const FILENAME: &str = "../data/complete.ged";
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse-gedcom");
+    group.measurement_time(Duration::from_secs(30));
+    group.bench_function("parse gedcom", |b| b.iter(|| parse_gedcom(FILENAME)));
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
